35a4df2ba2cd0350fe9562206330d1900dc6f47b
siggwer <jerome.sauvage@etu.unilim.fr>
2022-05-24 13:35:35 +0200

    update configuration

--- assets/front/js/app.js
+++ assets/front/js/app.js
@@ -6,10 +6,31 @@
  */
 
 // any CSS you import will output into a single css file (app.css in this case)
-import '../../styles/app.scss';
+import '../../front/css/custom.scss';
+import '../vendor/owlcarousel/assets/owl.carousel.css';
+import '../vendor/animate/animate.css';
+import '../../front/css/bootstrap.min.css';
+import '../../front/css/style.css';
 
 // start the Stimulus application
 //import './bootstrap';
 
 // import bootstrap
 import 'bootstrap';
+
+// import jquery
+const $ = require('jquery');
+require('bootstrap');
+
+// $(document).ready(function() {
+//     $('[data-toggle="popover"]').popover();
+// });
+
+// import javascript
+//import '../vendor/wow/wow';
+import '../vendor/easing/easing';
+import '../vendor/waypoints/waypoints.min';
+import '../vendor/counterup/counterup.min';
+import '../js/main';
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
--- webpack.config.js
+++ webpack.config.js
@@ -7,84 +7,98 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
 }
 
 //Encore
-    // directory where compiled assets will be stored
-    //.setOutputPath('public/build/')
-    // public path used by the web server to access the output path
-    //.setPublicPath('/build')
-    // only needed for CDN's or sub-directory deploy
-    //.setManifestKeyPrefix('build/')
+// directory where compiled assets will be stored
+//.setOutputPath('public/build/')
+// public path used by the web server to access the output path
+//.setPublicPath('/build')
+// only needed for CDN's or sub-directory deploy
+//.setManifestKeyPrefix('build/')
 
-    /*
-     * ENTRY CONFIG
-     *
-     * Each entry will result in one JavaScript file (e.g. app.js)
-     * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
-     */
-    //.addEntry('app', './assets/app.js')
+/*
+ * ENTRY CONFIG
+ *
+ * Each entry will result in one JavaScript file (e.g. app.js)
+ * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
+ */
+//.addEntry('app', './assets/app.js')
 
-    // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
-    //.enableStimulusBridge('./assets/controllers.json')
+// enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
+//.enableStimulusBridge('./assets/controllers.json')
 
-    // When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
-    //.splitEntryChunks()
+// When enabled, Webpack "splits" your files into smaller pieces for greater optimization.
+//.splitEntryChunks()
 
-    // will require an extra script tag for runtime.js
-    // but, you probably want this, unless you're building a single-page app
-    //.enableSingleRuntimeChunk()
+// will require an extra script tag for runtime.js
+// but, you probably want this, unless you're building a single-page app
+//.enableSingleRuntimeChunk()
 
-    /*
-     * FEATURE CONFIG
-     *
-     * Enable & configure other features below. For a full
-     * list of features, see:
-     * https://symfony.com/doc/current/frontend.html#adding-more-features
-     */
-    // .cleanupOutputBeforeBuild()
-    // .enableBuildNotifications()
-    // .enableSourceMaps(!Encore.isProduction())
-    // enables hashed filenames (e.g. app.abc123.css)
-    // .enableVersioning(Encore.isProduction())
-    //
-    // .configureBabel((config) => {
-    //     config.plugins.push('@babel/plugin-proposal-class-properties');
-    // })
+/*
+ * FEATURE CONFIG
+ *
+ * Enable & configure other features below. For a full
+ * list of features, see:
+ * https://symfony.com/doc/current/frontend.html#adding-more-features
+ */
+// .cleanupOutputBeforeBuild()
+// .enableBuildNotifications()
+// .enableSourceMaps(!Encore.isProduction())
+// enables hashed filenames (e.g. app.abc123.css)
+// .enableVersioning(Encore.isProduction())
+//
+// .configureBabel((config) => {
+//     config.plugins.push('@babel/plugin-proposal-class-properties');
+// })
 
-    // enables @babel/preset-env polyfills
-    // .configureBabelPresetEnv((config) => {
-    //     config.useBuiltIns = 'usage';
-    //     config.corejs = 3;
-    // })
+// enables @babel/preset-env polyfills
+// .configureBabelPresetEnv((config) => {
+//     config.useBuiltIns = 'usage';
+//     config.corejs = 3;
+// })
 
-    // enables Sass/SCSS support
-    //.enableSassLoader()
+// enables Sass/SCSS support
+//.enableSassLoader()
 
-    // enables Postcss support
-    //.enablePostCssLoader()
+// enables Postcss support
+//.enablePostCssLoader()
 
-    // uncomment if you use TypeScript
-    //.enableTypeScriptLoader()
+// uncomment if you use TypeScript
+//.enableTypeScriptLoader()
 
-    // uncomment if you use React
-    //.enableReactPreset()
+// uncomment if you use React
+//.enableReactPreset()
 
-    // uncomment to get integrity="..." attributes on your script & link tags
-    // requires WebpackEncoreBundle 1.4 or higher
-    //.enableIntegrityHashes(Encore.isProduction())
+// uncomment to get integrity="..." attributes on your script & link tags
+// requires WebpackEncoreBundle 1.4 or higher
+//.enableIntegrityHashes(Encore.isProduction())
 
-    // uncomment if you're having problems with a jQuery plugin
-    //.autoProvidejQuery()
+// uncomment if you're having problems with a jQuery plugin
+//.autoProvidejQuery()
 //;
 
 //module.exports = Encore.getWebpackConfig();
 
 // define the first configuration
 Encore
-    .setOutputPath('public/build/')
-    .setPublicPath('/build')
-    .addEntry('app', './assets/front/js/app.js')
+    .setOutputPath('public/front/build/')
+    .setPublicPath('/front/build')
+    .addEntry('front', './assets/front/js/app.js')
+    .copyFiles({
+        from: './assets/images/front',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
     .enableSingleRuntimeChunk()
     .enableSassLoader()
+    //.enablePostCssLoader()
     .enableSourceMaps(!Encore.isProduction())
+    .autoProvidejQuery()
 ;
 
 // build the first configuration
@@ -101,9 +115,23 @@ Encore
     .setOutputPath('public/admin/build/')
     .setPublicPath('/admin/build')
     .addEntry('back', './assets/admin/js/app.js')
+    .copyFiles({
+        from: './assets/images/back',
+
+        // optional target path, relative to the output dir
+        to: 'images/[path][name].[ext]',
+
+        // if versioning is enabled, add the file hash too
+        //to: 'images/[path][name].[hash:8].[ext]',
+
+        // only copy files matching this pattern
+        //pattern: /\.(png|jpg|jpeg)$/
+    })
     .enableSingleRuntimeChunk()
     .enableSassLoader()
+    //.enablePostCssLoader()
     .enableSourceMaps(!Encore.isProduction())
+    .autoProvidejQuery()
 ;
 
 // build the second configuration
